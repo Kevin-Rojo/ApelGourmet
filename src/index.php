80c196a91261c1bf8e9e3b84601afe56165cd51e
Kevin Rojo <kevin.rojoor@uanl.edu.mx>
2022-05-03 16:59:07 -0500

    Se agrega bootstrap al Proyecto Base

---
+++ src/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ApelGourmet</title>
+    <link rel="stylesheet" href="Bootstrap\bootstrap-5.1.3-dist\css\bootstrap.css">
+</head>
+<body>
+    <nav class="nav justify-content-center|justify-content-end">
+      <a class="nav-link active" href="#">Active link</a>
+      <a class="nav-link" href="#">Link</a>
+      <a class="nav-link disabled" href="#">Disabled link</a>
+    </nav>
+    <div class="container-fluid">
+
+    </div>
+
+    <!-- Scripts Bootstrap -->
+    <script src="Bootstrap\bootstrap-5.1.3-dist\js\bootstrap.js"></script>
+    <script src="Bootstrap\bootstrap-5.1.3-dist\js\bootstrap.min.js"></script>
+</body>
+</html>
