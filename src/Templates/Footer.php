9fd5ae618f53a4be9e386de2d8446aafd5fa54ed
Kevin Rojo <kevin.rojoor@uanl.edu.mx>
2022-05-16 18:00:51 -0500

    Actualizacion 2 Footer

--- src/Templates/Footer.php
+++ src/Templates/Footer.php
@@ -1,43 +1,44 @@
 <footer class="py-4" style="background-color: #F9F3EE;">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-xl-6 col-md-12">
                 <div class="row">
-                    <div class="col-md-4">
-                        <div style="border-right: 1px solid grey; height: 100%;">
-                            <h1>ApelGourmet</h1>
+                    <div class="col text-center">
+                        <div class="text-align-center" style="border-right: 1px solid grey; height: 100%;">
+                            <h1 class="">ApelGourmet</h1>
                         </div>
                     </div>
-                    <div class="col-md-6 px-4">
+                    <div class="col text-center">
                     <h6> About Company</h6>
                     <p>But horizontal lines can only be a full pixel high.</p>
-                    <a href="#" class="btn-footer"> More Info </a><br>
-                    <a href="#" class="btn-footer"> Contact Us</a>
+                    <a href="#" class="link-secondary"> More Info </a><br>
+                    <a href="#" class="link-secondary"> Contact Us</a>
                     </div>
                 </div>
             </div>
-            <div class="col-md-6">
+            <div class="col-xl-6 col-md-12">
                 <div class="row">
-                    <div class="col-md-6 px-4">
-                        <h6>Contenido</h6>
-                        <div class="row ">
-                            <div class="col-md-6">
+                    <div class="col-xl-6 col-md-6">
+                        <h6 class="">Contenido</h6>
+                        <div class="row">
+                            <div class="col-6">
                                 <ul>
-                                    <li> <a href="#"> Inicio</a> </li>
-                                    <li> <a href="#"> Catalogo</a> </li>
-                                    <li> <a href="#"> Nosotros</a> </li>
+                                    <li> <a href="#" class="link-secondary"> Inicio</a> </li>
+                                    <li> <a href="#" class="link-secondary"> Catalogo</a> </li>
+                                    <li> <a href="#" class="link-secondary"> Nosotros</a> </li>
                                 </ul>
                             </div>
-                            <div class="col-md-6 px-4">
+                            <div class="col-6">
                                 <ul>
-                                    <li> <a href="#"> Cab Faciliy</a> </li>
-                                    <li> <a href="#"> Fax</a> </li>
-                                    <li> <a href="#"> Terms</a> </li>
+                                    <li> <a href="#" class="link-secondary"> Cab Faciliy</a> </li>
+                                    <li> <a href="#" class="link-secondary"> Fax</a> </li>
+                                    <li> <a href="#" class="link-secondary"> Terms</a> </li>
                                 </ul>
                             </div>
                         </div>
-                        </div>
-                        <div class="col-md-6 ">
+                    </div>
+                        
+                    <div class="col">
                         <h6>Contacto</h6>
                         <div class="social">
                             <a href="#"><i class="bi bi-facebook"></i></i></a>
@@ -48,8 +49,38 @@
                             <input type="button" value="Go" >
                         </form>
                     </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
+
 </footer>
+<div class="copyright">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <span>Copyright © 2022, All Right Reserved ApelGourmet</span>
+            </div>
+            <!-- End Col -->
+            <div class="col-md-6">
+                <div class="copyright-menu">
+                    <ul>
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">Terms</a>
+                        </li>
+                        <li>
+                            <a href="#">Privacy Policy</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- src/index.php
+++ src/index.php
@@ -6,13 +6,14 @@
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>ApelGourmet</title>
       <link rel="stylesheet" href="..\Bootstrap\bootstrap-5.1.3-dist\css\bootstrap.min.css">
-  </head>
+      <link rel="stylesheet" href="..\css\main.css">
+    </head>
   <body>
 
   <?php include('Templates\nav.php')?>
 
-  <div class="container h-100 text-center d-flex">
-    <h1>CONTENIDO</h1>
+  <div class="container text-center position-relative" style="height: 650px;">
+    <h1 class="position-absolute top-50 start-50 translate-middle" style="heigh: 2000px;">CONTENIDO</h1>
   </div>
 
   <?php include('Templates\Footer.php')?>
